0da376ea96386221d510b1ed27b58df117c3532d
kartik <kartikjoshi842003+git@gmail.com>
2024-03-11 16:18:25 +0530

    some output changes

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
 
 public class Bank {
@@ -27,6 +29,7 @@ public class Bank {
         }
     }
 
+
     public BankAccount searchAccount(long accountNumber){
         for(BankAccount a : bankaccounts){
             if(a.getAcc_number() == (accountNumber)){
@@ -36,9 +39,11 @@ public class Bank {
         return null;
     }
 
+
     public List<BankAccount> showAllAccounts(){
         return bankaccounts;
     }
+
 
     public void depositMoney(int cashToDeposit, long accountNumber){
         BankAccount account = searchAccount(accountNumber);
@@ -46,6 +51,7 @@ public class Bank {
         currBalance = cashToDeposit+currBalance;
         account.setAcc_balance(currBalance);
     }
+
 
     public void withdrawMoney(int cashToWithdraw, long accountNumber){
         BankAccount account = searchAccount(accountNumber);
@@ -57,5 +63,22 @@ public class Bank {
             System.out.println("INSUFFICIENT BALANCE \n unable to process");
         }
     }
-    
+
+
+    public void menu(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------------- Welcome to the Bank ---------------");
+        System.out.println("======= Choose an option ======= ");
+        System.out.println("\t 1. Create an account");
+        System.out.println("\t 2. Delete an account");
+        System.out.println("\t 2. Search your account");
+        System.out.println("\t 3. Exit");
+
+        int in = sc.nextInt();
+        sc.nextLine();       
+        
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.sql.SQLOutput;
+ 
 
-public class Main {
+
+class Main{
+   
     public static void main(String[] args) {
-        Bank Sbi = new Bank();
-
-        BankAccount firstAccount = new BankAccount(982372902173032l, "kartik", 4000);
-        Sbi.addAccount(firstAccount);
-        System.out.println(Sbi.showAllAccounts());
-        Sbi.depositMoney(250000, 982372902173032l);
-        System.out.println(firstAccount.getAcc_balance());
-
+        Bank sbi = new Bank();
+        sbi.menu();
+       System.out.println(sbi.showAllAccounts());
+    
     }
 }
